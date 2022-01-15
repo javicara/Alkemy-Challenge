fc33bfa87592bbe2a3ac3ad63ec4f69ef56a8123
javicara <javiercaraballo97@gmail.com>
2022-01-15 00:28:45 -0300

    adding model Characters_movies

---
+++ app/models/Characters_Movies.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = require("../util/database");
+
+const CharacterMovies = db.define(
+  "characters_movies",
+  {
+    movieMovieId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+
+    },
+    characterCharacterId:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+  },
+
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = CharacterMovies;
